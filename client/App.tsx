46ae23ff8457a8d4883e796d72a01079f7f42df3
Tyler Shelton <tdshelton@gmail.com>
2024-01-09 15:58:36 -0800

    Update text markup

--- client/App.tsx
+++ client/App.tsx
@@ -3,7 +3,7 @@ import { JSX } from 'react/jsx-runtime';
 const App = (): JSX.Element => {
   return (
     <main>
-      <p>A cat gif stored at __dirname/static/cat_meme.gif --</p>
+      <p>A cat gif stored at <pre>__dirname/static/cat_meme.gif</pre> --</p>
       <p>app.use(&lt;mount path&gt;, express.static(&lt;root&gt;))</p>
       <p>-- will be served at hostname:port/&lt;result&gt;</p>
     </main>
