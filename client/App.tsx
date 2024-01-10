bc574c22e4c2f02fed2e0cbb05ddcb727860302f
Tyler Shelton <tdshelton@gmail.com>
2024-01-09 16:08:04 -0800

    Make all paths/code statements monospace

--- client/App.tsx
+++ client/App.tsx
@@ -3,9 +3,9 @@ import { JSX } from 'react/jsx-runtime';
 const App = (): JSX.Element => {
   return (
     <main>
-      <p>A cat gif stored at <pre>__dirname/static/cat_meme.gif</pre> --</p>
-      <p>app.use(&lt;mount path&gt;, express.static(&lt;root&gt;))</p>
-      <p>-- will be served at hostname:port/&lt;result&gt;</p>
+      <p>A cat gif stored at <code>__dirname/static/cat_meme.gif</code> --</p>
+      <pre>app.use(&lt;mount path&gt;, express.static(&lt;root&gt;))</pre>
+      <p>-- will be served at <code>hostname:port/&lt;result&gt;</code></p>
     </main>
   );
 };
