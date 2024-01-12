e55256148fccce1d34a691a6cd8da0afa411bb81
Tyler Shelton <tdshelton@gmail.com>
2024-01-11 23:44:05 -0800

    Work input fields into mockup

--- client/App.tsx
+++ client/App.tsx
@@ -1,11 +1,77 @@
+import { css } from '@emotion/react';
 import { JSX } from 'react/jsx-runtime';
+import { TextField } from '@mui/material';
+
+const globalStyle = css({
+  fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
+  fontWeight: 400,
+  fontSize: '1rem',
+  color: 'rgba(0, 0, 0, 0.87)'
+});
+
+const serverSetupStyle = css({
+  margin: '2rem 1rem',
+  display: 'flex',
+  '& p, pre': {
+    whiteSpace: 'nowrap',
+    margin: 'auto 0.5rem'
+  }
+});
 
 const App = (): JSX.Element => {
   return (
-    <main>
-      <p>A cat gif stored at <code>__dirname/static/cat_meme.gif</code> --</p>
-      <pre>app.use(&lt;mount path&gt;, express.static(&lt;root&gt;))</pre>
-      <p>-- will be served at <code>hostname:port/&lt;result&gt;</code></p>
+    <main css={globalStyle}>
+      <section id='server_setup' css={serverSetupStyle}>
+        <p>Given a server listening at</p>
+        <TextField
+          id='server_hostname'
+          label='hostname/ip'
+          variant='outlined'
+          defaultValue='localhost'
+          size='small'
+        />
+        <p>:</p>
+        <TextField
+          id='sever_port'
+          label='port'
+          variant='outlined'
+          defaultValue='80'
+          css={{ width: '6rem' }}
+          size='small'
+        />
+        <p>, a cat gif stored at <code>__dirname/</code></p>
+        <TextField
+          id="local_filepath"
+          label="filepath"
+          variant="outlined"
+          defaultValue="static"
+          size='small'
+        />
+        <p>--</p>
+      </section>
+      <section id='express_static_setup' css={serverSetupStyle}>
+        <pre>app.use(</pre>
+        <TextField
+          id='express_static_mount_path'
+          label='mount path'
+          variant='outlined'
+          defaultValue='/'
+          css={{ width: '10rem'}}
+          size='small'
+        />
+        <pre>, express.static(</pre>
+        <TextField
+          id='express_static_root'
+          label='root'
+          variant='outlined'
+          defaultValue='__dirname, static'
+          size='small'
+        />
+        <pre>))</pre>
+      </section>
+      <section id='result' css={serverSetupStyle}>
+        <p>-- would be served at <code>hostname:port/&lt;result&gt;/cat_meme.gif</code>.</p>
+      </section>
     </main>
   );
 };
