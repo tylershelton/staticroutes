0855d77f691f34b9c893b4b22ae71e72e436c829
Tyler Shelton <tdshelton@gmail.com>
2024-01-17 11:15:51 -0800

    Add new action type for modifying the http route

--- client/types/RouteActions.ts
+++ client/types/RouteActions.ts
@@ -1,5 +1,6 @@
 type RouteAction =
   | { type: 'changed_hostname'; hostname: string }
+  | { type: 'changed_httpRoute'; httpRoute: string }
   | { type: 'changed_port'; port: string }
   | { type: 'changed_staticdir'; staticDir: string };
 
