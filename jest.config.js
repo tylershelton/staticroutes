68b5cba5817599c5246abaa7d4fff5a7e17e8e42
Tyler Shelton <tdshelton@gmail.com>
2024-01-18 13:46:49 -0800

    Make jest use ESM for TS and JSX files

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@
 export default {
   preset: 'ts-jest/presets/js-with-ts-esm',
   // preset: 'ts-jest/presets/js-with-babel-esm',
+  extensionsToTreatAsEsm: ['.ts', '.tsx', '.jsx'],
   testEnvironment: 'jsdom'
 };
