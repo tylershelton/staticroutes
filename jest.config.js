566fbd5536dc0bcf5884b93fc4a9ca112cf38f0f
Tyler Shelton <tdshelton@gmail.com>
2024-01-17 17:13:05 -0800

    Configure Jest for typescript support

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+  preset: 'ts-jest/presets/js-with-ts-esm',
+  // preset: 'ts-jest/presets/js-with-babel-esm',
+  testEnvironment: 'jsdom'
+};
