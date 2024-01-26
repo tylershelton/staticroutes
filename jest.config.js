2f91ace3759d730d57369e3aba3490a4c0e88144
Tyler Shelton <tdshelton@gmail.com>
2024-01-25 23:36:00 -0800

    Cull duplicate config items, add regex to only test `test.ts` files (and the like)

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
   preset: 'ts-jest/presets/js-with-ts-esm',
-  // preset: 'ts-jest/presets/js-with-babel-esm',
-  extensionsToTreatAsEsm: ['.ts', '.tsx', '.jsx'],
-  testEnvironment: 'jsdom'
+  testEnvironment: 'jsdom',
+  testRegex: '/__tests__/.*test\\.(j|t)sx?$'
 };
