48312b93a8339567c70e7f95effbd50452edfb7e
Tyler Shelton <tdshelton@gmail.com>
2024-02-03 14:55:09 -0800

    Use sx prop to define margins, rather than inline custom CSS

--- client/components/ExpressStatic.tsx
+++ client/components/ExpressStatic.tsx
@@ -3,14 +3,14 @@ import { useContext } from 'react';
 import { RouteContext, RouteDispatchContext } from '../contexts/RouteContext';
 import { useDispatchFromContext } from '../lib/dispatchHelpers';
 import FlowIndicator from './FlowIndicator';
-const serverSetupStyle = css({
-  margin: '2rem 0',
-  // display: 'flex',
-  // '& p, pre': {
-  //   whiteSpace: 'nowrap',
-  //   margin: 'auto 0.5rem'
-  // }
-});
+
+// const serverSetupStyle = css({
+//   display: 'flex',
+//   '& p, pre': {
+//     whiteSpace: 'nowrap',
+//     margin: 'auto 0.5rem'
+//  }
+// });
 
 const ExpressStatic = (): JSX.Element => {
   const { httpRoute, staticDir } = useContext(RouteContext);
@@ -32,27 +32,28 @@ const ExpressStatic = (): JSX.Element => {
 
   return (
     <Stack>
-      <pre>app.use(</pre>
-      <TextField
-        id='express_static_mount_path'
-        label='mount path'
-        variant='outlined'
-        value={httpRoute}
-        css={{ width: '10rem'}}
-        size='small'
-        onChange={handleChangeHttpRoute}
-      />
-      <pre>, express.static(path.resolve(__dirname,</pre>
-      <TextField
-        id='express_static_root'
-        label='root'
-        variant='outlined'
-        value={staticDir}
-        size='small'
-        onChange={handleChangeStaticDir}
-      />
-      <pre>))</pre>
-    </Paper>
+      <Paper sx={{ p: 2 }} id='express_static_setup'>
+        <pre>app.use(</pre>
+        <TextField
+          id='express_static_mount_path'
+          label='mount path'
+          variant='outlined'
+          value={httpRoute}
+          css={{ width: '10rem' }}
+          size='small'
+          onChange={handleChangeHttpRoute}
+        />
+        <pre>, express.static(path.resolve(__dirname,</pre>
+        <TextField
+          id='express_static_root'
+          label='root'
+          variant='outlined'
+          value={staticDir}
+          size='small'
+          onChange={handleChangeStaticDir}
+        />
+        <pre>))</pre>
+      </Paper>
       <FlowIndicator />
     </Stack>
   );
