e5924e45c2774ff9c9fd81bc4c762ecf200e5b94
Tyler Shelton <tdshelton@gmail.com>
2024-02-03 13:04:36 -0800

    Remove CSS

--- client/App.tsx
+++ client/App.tsx
@@ -11,28 +11,20 @@ import routeReducer from './reducers/RouteReducer';
 import ServerConf from './components/ServerConf';
 import ExpressStatic from './components/ExpressStatic';
 import Output from './components/Output';
-import { initialRouteState } from './initialState';
-
-const globalStyle = css({
-  fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
-  fontWeight: 400,
-  fontSize: '1rem',
-  color: 'rgba(0, 0, 0, 0.87)'
-});
 
 const App = (): JSX.Element => {
   const [routeState, dispatch] = useReducer(routeReducer, initialRouteState);
 
   return (
-      <RouteContext.Provider value={routeState}>
-        <RouteDispatchContext.Provider value={dispatch}>
+    <RouteContext.Provider value={routeState}>
+      <RouteDispatchContext.Provider value={dispatch}>
         <Stack>
           <ServerConf />
           <ExpressStatic />
           <Output />
         </Stack>
-        </RouteDispatchContext.Provider>
-      </RouteContext.Provider>
+      </RouteDispatchContext.Provider>
+    </RouteContext.Provider>
   );
 };
 
