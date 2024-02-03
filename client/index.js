a08f51f9d02aaa84eb8a7cf0ef3a4fbea795a378
Tyler Shelton <tdshelton@gmail.com>
2024-02-03 13:04:56 -0800

    Add theming, StrictMode

--- client/index.js
+++ client/index.js
@@ -1,6 +1,30 @@
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 
 import App from './App.tsx';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#88C0D0',
+    },
+    background: {
+      default: '#ECEFF4',
+      paper: '#E5E9F0',
+    },
+    text: {
+      primary: '#2E3440',
+    },
+  },
+});
+
 const root = createRoot(document.getElementById('app'));
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
